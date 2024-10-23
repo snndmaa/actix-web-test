50c8d200305d1e2a9fcd3e9b53abb095830dc30a
sndmaa <sanclaw55@gmail.com>
2024-10-24 01:49:06 +0400

    routing and state

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+mod zzz; // Declare the zzz module
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use crate::zzz::test_route::test; // Import the test function from test_route
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/state")]
+async fn get_state(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(get_state)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::scope("/app")
+                    .route("/index.html", web::get().to(test))
+            )
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Tester"),
+            }))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
---
+++ src/zzz/test_route.rs
@@ -0,0 +1,5 @@
+use actix_web::{HttpResponse, Responder};
+
+pub async fn test() -> impl Responder {
+    HttpResponse::Ok().body("Test route")
+}
